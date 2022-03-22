18eac28bce10f98c4a68f04622dba75de97d2a30
Gert Drapers <gert.drapers@live.com>
2022-03-21 21:21:47 -0700

    update CI

--- magefile.go
+++ magefile.go
@@ -4,15 +4,18 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/aserto-dev/mage-loot/common"
 	"github.com/aserto-dev/mage-loot/deps"
+	"github.com/pkg/errors"
 )
 
-func init() {
-	// Set private repositories
-	os.Setenv("GOPRIVATE", "github.com/aserto-dev")
+// Generate generates all code.
+func Generate() error {
+	return common.Generate()
 }
 
 // Lint runs linting for the entire project.
@@ -25,23 +28,59 @@ func Test() error {
 	return common.Test()
 }
 
-// Build builds all binaries in ./cmd.
+// Build all binaries in ./cmd.
 func Build() error {
 	return common.BuildReleaser()
 }
 
-// Release releases the project.
+// Release the project.
 func Release() error {
-	return common.Release()
+	if os.Getenv("GITHUB_TOKEN") == "" {
+		return fmt.Errorf("GITHUB_TOKEN environment variable is undefined")
+	}
+
+	if os.Getenv("HOMEBREW_TAP") == "" {
+		return fmt.Errorf("HOMEBREW_TAP environment variable is undefined")
+	}
+
+	if os.Getenv("GOOGLE_APPLICATION_CREDENTIALS") == "" {
+		return fmt.Errorf("GOOGLE_APPLICATION_CREDENTIALS environment variable is undefined")
+	}
+
+	if err := writeVersion(); err != nil {
+		return err
+	}
+
+	return common.Release("--rm-dist")
 }
 
 // BuildAll builds all binaries in ./cmd for
 // all configured operating systems and architectures.
 func BuildAll() error {
-	return common.BuildAllReleaser("--snapshot")
+	return common.BuildAllReleaser("--rm-dist", "--snapshot")
 }
 
 // Deps installs all dependencies required to build the project.
 func Deps() {
 	deps.GetAllDeps()
+}
+
+func writeVersion() error {
+	version, err := exec.Command("git", "describe", "--tags").Output()
+	if err != nil {
+		return errors.Wrap(err, "failed to get current git tag")
+	}
+
+	file, err := os.Create("VERSION.txt")
+	if err != nil {
+		return errors.Wrap(err, "failed to create version file")
+	}
+
+	defer file.Close()
+
+	if _, err := file.Write(version); err != nil {
+		return errors.Wrap(err, "failed to write to version file")
+	}
+
+	return file.Sync()
 }
